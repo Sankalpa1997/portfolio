d58377b87c6450f235f7747443696255b0fc48a0
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-18 23:00:38 +0100

    Background gradient mouse effect added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
 import Content from "./Content";
 import Header from "./Header";
+import MouseGradient from "./MouseGradient";
 import styles from "./layout.module.css";
 
 export default function Layout() {
   return (
-    <div className={styles.layout}>
-      <Header />
-      <Content />
+    <div className={styles.fullScreenContainer}>
+      <MouseGradient />
+      <div className={styles.layout}>
+        <Header />
+        <Content />
+      </div>
     </div>
   );
 }
---
+++ src/components/MouseGradient.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import styles from './mouseGradient.module.css';
+
+export default function MouseGradient() {
+  const [mouseXpercentage, setMouseXpercentage] = useState(50);
+  const [mouseYpercentage, setMouseYpercentage] = useState(50);
+  const [mouseXpercentageM, setMouseXpercentageM] = useState(50);
+  const [mouseYpercentageM, setMouseYpercentageM] = useState(50);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      
+      setMouseXpercentage(Math.round(100 - (event.pageX / windowWidth * 100)));
+      setMouseYpercentage(Math.round(100 - (event.pageY / windowHeight * 100)));
+      setMouseXpercentageM(Math.round(event.pageX / windowWidth * 100));
+      setMouseYpercentageM(Math.round(event.pageY / windowHeight * 100));
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <>
+      <div 
+        className={styles.element} 
+        style={{ backgroundPosition: `${mouseXpercentage}% ${mouseYpercentage}%` }}
+      ></div>
+      <div 
+        className={styles.mouseGradient} 
+        style={{ 
+          background: `radial-gradient(600px at ${mouseXpercentageM}% ${mouseYpercentageM}%, rgba(29, 78, 216, 0.15), transparent 80%)`
+        }}
+      ></div>
+    </>
+  );
+}
