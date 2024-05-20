f06209de73f353a7d5dcc2fd3381de2d7d143bbe
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-20 03:19:46 +0100

    Social icons added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import Navbar from "./Navbar";
+import SocialIcons from "./SocialIcons";
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className="text-white">Sankalpa Senevirathne</h1>
-      <h2 className="text-white w-500">Full Stack Developer</h2>
-      <p>Web Wizard at Work : Weaving Imaginations Into Digital Experiences</p>
-      <Navbar />
+      <div>
+        <h1 className="text-white">Sankalpa Senevirathne</h1>
+        <h2 className="text-white w-500">Full Stack Developer</h2>
+        <p>
+          Web Wizard at Work : Weaving Imaginations Into Digital Experiences
+        </p>
+        <Navbar />
+      </div>
+      <div>
+        <SocialIcons />
+      </div>
     </header>
   );
 }
