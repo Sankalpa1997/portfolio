369f8dc6dd91da057d47becef4f27790a0004d6f
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-22 01:25:56 +0100

    About component added

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <section id="about">
+      <div>
+        <p className="relaxed">
+          Passionate full-stack developer with a solid foundation in PHP Laravel
+          and React.js, complemented by expert-level skills in custom WordPress
+          and Shopify development, alongside Webflow proficiency. Experienced in
+          crafting Android apps using Java and iOS apps using Swift.
+          Additionally, proficient in cross-platform app development with
+          Flutter. Let's connect and share the journey of creativity and
+          innovation in the digital world.
+        </p>
+        <p className="relaxed">
+          I’m currently learning about SEO & Digital Marketing as a
+          self-interest. I'm mostly active within the Software Engineering,
+          ReactJs development & cross platform mobile app development
+          communities. Fun facts: I love to play the guitar and play video games
+          when I need a break ...
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,21 +1,9 @@
+import About from "./About";
 import styles from "./content.module.css";
 export default function Content() {
   return (
     <div className={styles.content}>
-      <p className="relaxed">
-        Passionate full-stack developer with a solid foundation in PHP Laravel
-        and React.js, complemented by expert-level skills in custom WordPress
-        and Shopify development, alongside Webflow proficiency. Experienced in
-        crafting Android apps using Java and iOS apps using Swift. Additionally,
-        proficient in cross-platform app development with Flutter. Let's connect
-        and share the journey of creativity and innovation in the digital world.
-      </p>
-      <p className="relaxed">
-        I’m currently learning about SEO & Digital Marketing as a self-interest.
-        I'm mostly active within the Software Engineering, ReactJs development &
-        cross platform mobile app development communities. Fun facts: I love to
-        play the guitar and play video games when I need a break ...
-      </p>
+      <About />
     </div>
   );
 }
