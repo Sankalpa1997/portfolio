c3de82de1335dd2c79295f4d511e639f5051db62
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-22 01:31:20 +0100

    Experience component added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,11 @@
 import About from "./About";
+import Experience from "./Experience";
 import styles from "./content.module.css";
 export default function Content() {
   return (
     <div className={styles.content}>
       <About />
+      <Experience />
     </div>
   );
 }
