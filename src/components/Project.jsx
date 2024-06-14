531bf2c5b69030d824a9bef33b41babcdba587d7
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-06-14 18:58:21 +0100

    Project card styles added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -57,8 +57,22 @@ export default function Project() {
               <span className={styles.featuredTitle}>Featured Project</span>
               <h3>{project.title}</h3>
               <h4 className={styles.projectCompanyName}>@{project.companyName}</h4>
-              <div className={styles.projectDescriptionWrapper}>{project.description}</div>
-              <div dangerouslySetInnerHTML={{ __html: project.desc }} />
+              <div className={styles.projectInfoWrapper}>
+                <div className={styles.projectDescriptionContainer}>
+                  <div>{project.description}</div>
+                  <div className="iconButton">
+                    <span className="">Visit Project</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="92-Arrow Right">
+                      <path fill="currentColor" d="M16 32a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm0-30a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"/>
+                      <path fill="currentColor" d="M13.71 24.71 12.3 23.3l7.29-7.3-7.3-7.29L13.7 7.3l8 8a1 1 0 0 1 0 1.41z"/></g></svg>
+                  </div>
+                </div>
+                <div className={styles.projectImageContainer}>
+                  <img src="./src/assets/civiq.png" alt={`${project.title} Screenshot`} />
+                </div>
+              </div>
+
+              {/* <div dangerouslySetInnerHTML={{ __html: project.desc }} /> */}
 
               <div className={styles.techStack}>
                 {project.techStack && project.techStack.map((tech, i) => (
@@ -70,9 +84,6 @@ export default function Project() {
                   <span key={i} className="pill">{plugin}</span>
                 ))}
               </div>
-            </div>
-            <div className={styles.projectImageContainer}>
-              <img src="" alt="" />
             </div>
           </a>
         ))}
