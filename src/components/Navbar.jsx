e31ce79561c6a49e688d5ca5855a859a865d6872
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-06-20 03:09:56 +0100

    navbar scroll functionality added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,36 @@
+import React, { useEffect } from 'react';
 import styles from "./navbar.module.css";
+
 export default function Navbar() {
+
+  const handleLinkClick = (event, id) => {
+    event.preventDefault();
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <a href="">
+          <a href="#about" onClick={(event) => handleLinkClick(event, 'about')}>
             <span className={styles.navIndicator}></span>
             <span>About</span>
           </a>
         </li>
         <li>
-          <a href="">
+          <a href="#experience"  onClick={(event) => handleLinkClick(event, 'experience')}>
             <span className={styles.navIndicator}></span>
             <span>Experience</span>
           </a>
         </li>
         <li>
-          <a href="">
+          <a href="#project"  onClick={(event) => handleLinkClick(event, 'project')}>
             <span className={styles.navIndicator}></span>
             <span>Projects</span>
           </a>
