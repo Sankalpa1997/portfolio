7936bcbc0502d997a23719d975dd54eb19cca260
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-08-28 17:46:33 +0100

    re-initialized GA4 in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ReactGA from 'react-ga4';
+
+const measurementId = 'G-21PSSKRKH5'; // Replace with your GA Measurement ID
+
+// Initialize Google Analytics
+ReactGA.initialize(measurementId);
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
