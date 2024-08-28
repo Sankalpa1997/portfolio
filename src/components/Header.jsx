6924954cc4ad447c2af6ba05951a4dc908f77321
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-08-28 03:12:51 +0100

    function issue fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
           <p className="relaxed">
             I transform ideas into seamless and captivating digital experiences.
           </p>
-          <a className="btn" href="https://www.linkedin.com/in/sankalpasenevirathne/" target="_blank" onClick={handleLinkedInClick}>Let's Connect</a>
+          <a className="btn" href="https://www.linkedin.com/in/sankalpasenevirathne/" target="_blank" onClick={handleConnectClick}>Let's Connect</a>
         </div>
         <Navbar />
       </div>
