e3536ba2a460051c839420687e15cf4f9b354e40
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-08-28 02:44:22 +0100

    GA4 initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Layout from "./components/Layout";
+import ReactGA from 'react-ga4';
 
 function App() {
+
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize('G-21PSSKRKH5'); // Replace with your Google Analytics Measurement ID
+
+    // Track the initial page load
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
+  }, []);
+
   return (
     <>
       <Layout />
